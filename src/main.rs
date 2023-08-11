9f10fe3c828904a2f17db508a5b3fb7b6a1beacd
Max Perea Düring <maxpereaduring@gmail.com>
2023-08-11 10:58:37 +0200

    Use custom dockerfile

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
-/*--------------------------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
- *-------------------------------------------------------------------------------------------------------------*/
-
 fn main() {
-    let name = "VS Code Remote - Containers";
+    let name = "Dev Container";
     println!("Hello, {}!", name);
 }
